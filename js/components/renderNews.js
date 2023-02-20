de27e77cba8cde63bd8269516fca2f7510130dcd
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-02-20 16:35:57 +0100

    added the files with the module functions

---
+++ js/components/renderNews.js
@@ -0,0 +1,14 @@
+export function renderNews(newsToRender) {
+  const containerNews = document.querySelector(".container-news");
+
+  containerNews.innerHTML = "";
+
+  newsToRender.forEach(function (news) {
+    containerNews.innerHTML += `<div class="single-news">
+                                    <h4>${news.title}</h4>
+                                    <p>${news.summary}</p>
+                                    <p>${news.author}</p>
+                                    <i class="fa-regular fa-heart"></i>
+                                </div>`;
+  });
+}
