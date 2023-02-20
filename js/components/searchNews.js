837e431c0ed6959413b63ad3d0f8ebed8f571bdd
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-02-21 00:55:11 +0100

    wrote the function searchNews

---
+++ js/components/searchNews.js
@@ -0,0 +1,19 @@
+import { renderNews } from "./renderNews.js";
+
+export function searchNews(news) {
+  const searchInput = document.querySelector(".search-input");
+
+  function searchTitles(event) {
+    const searchedValue = event.target.value.trim().toLowerCase();
+
+    const filteredNews = news.filter(function (item) {
+      if (item.title.toLowerCase().includes(searchedValue)) {
+        return true;
+      }
+    });
+
+    renderNews(filteredNews);
+  }
+
+  searchInput.addEventListener("keyup", searchTitles);
+}
