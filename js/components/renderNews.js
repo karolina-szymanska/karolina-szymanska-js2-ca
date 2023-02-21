944dba22d2053372ba62347c9807d2ffe720c506
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-02-21 01:10:16 +0100

    added a class to 2nd p element

--- js/components/renderNews.js
+++ js/components/renderNews.js
@@ -7,7 +7,7 @@ export function renderNews(newsToRender) {
     containerNews.innerHTML += `<div class="single-news">
                                     <h4>${news.title}</h4>
                                     <p>${news.summary}</p>
-                                    <p>${news.author}</p>
+                                    <p class="author">-- ${news.author}</p>
                                     <i class="fa-regular fa-heart"></i>
                                 </div>`;
   });
