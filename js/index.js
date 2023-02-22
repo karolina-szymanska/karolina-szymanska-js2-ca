8922579cb3c3ba4f1477ce9c58058b8d175368a2
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-02-22 15:14:45 +0100

    added some comments on ls

--- js/index.js
+++ js/index.js
@@ -5,10 +5,22 @@ import displayMessage from "./components/displayMessage.js";
 
 const newsUrl = baseUrl + "articles";
 
+// const myVariable = "hello there";
+// localStorage.setItem("var1", myVariable);
+// const variable1 = localStorage.getItem("var1");
+// console.log(variable1);
+
+// const colours = ["green", "yellow", "orange"];
+// localStorage.setItem("savedColours", JSON.stringify(colours));
+// const gottenColours = localStorage.getItem("savedColours");
+// const parsedColours = JSON.parse(gottenColours);
+// console.log(parsedColours);
+
 (async function () {
   try {
     const response = await fetch(newsUrl);
     const results = await response.json();
+    // console.table(results);
 
     renderNews(results);
     searchNews(results);
@@ -21,3 +33,6 @@ const newsUrl = baseUrl + "articles";
     );
   }
 })();
+
+const heartButtons = document.querySelectorAll(".single-news i.fa-heart");
+console.log(heartButtons);
