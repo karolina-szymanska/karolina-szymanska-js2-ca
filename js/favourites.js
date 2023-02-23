38ac013f82b7ca2a09fc81c48af7b6fb50cfb9fd
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-02-23 04:28:40 +0100

    added button to clear LocalStorage; writing the clear function in progress

--- js/favourites.js
+++ js/favourites.js
@@ -3,9 +3,11 @@ import displayMessage from "./components/displayMessage.js";
 
 (function () {
   const favourites = getExistingFavourites();
+  const clearButton = document.querySelector("#clear-button");
 
   if (favourites.length === 0) {
     displayMessage("message", "Add your favourites", ".container-favourites");
+    clearButton.style.display = "none";
   }
 
   const containerFavourites = document.querySelector(".container-favourites");
@@ -18,4 +20,10 @@ import displayMessage from "./components/displayMessage.js";
                                         <i class="fa-solid fa-heart"></i>
                                     </div>`;
   });
+
+  clearButton.addEventListener("click", clearLocalStorage);
 })();
+
+function clearLocalStorage() {
+  localStorage.clear();
+}
